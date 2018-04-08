3e4375a425952352abb5af147c9442fc02adf79a
Brian Cappello <briancappello@gmail.com>
2018-04-07 20:26:43 -0400

    update reqeuirements

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
-def read_requirements(filename):
-    def is_pkg(line):
-        return line and not line.startswith(('--', 'git', '#'))
-
-    with open(filename, encoding='utf-8') as f:
-        return [line for line in f.read().splitlines() if is_pkg(line)]
-
-
 setup(
     name='Flask API Bundle',
     version='0.1.0',
@@ -24,16 +16,26 @@ setup(
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=find_packages(include=['flask_api_bundle']),
-    install_requires=read_requirements('requirements.txt'),
+    packages=find_packages(exclude=['docs', 'tests']),
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.6',
+    install_requires=[
+        'flask-controller-bundle>=0.2.0',
+        'flask-marshmallow>=0.8.0',
+        'flask-sqlalchemy-bundle>=0.2.0',
+        'flask-unchained>=0.2.0',
+        'marshmallow>=2.13.6',
+        'marshmallow-sqlalchemy>=0.13.1',
+    ],
     entry_points={
         'pytest11': [
             'flask_api_bundle = flask_api_bundle.pytest',
