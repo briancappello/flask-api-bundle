98a53c68359b4b7f6948825b73ca512ef875c4eb
Brian Cappello <briancappello@gmail.com>
2018-07-16 16:22:15 -0600

    release 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Flask API Bundle',
-    version='0.2.0',
+    version='0.2.1',
     description='Adds RESTful API support to Flask Unchained',
     long_description=long_description,
     url='https://github.com/briancappello/flask-api-bundle',
